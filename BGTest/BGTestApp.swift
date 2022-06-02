f804082c1f97bcc24873edb3a9f00442dd866a95
Amr Yousef <amr.farid140@gmail.com>
2022-06-02 09:52:38 +0100

    Initial Commit

---
+++ BGTest/BGTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGTestApp.swift
+//  BGTest
+//
+//  Created by Amr Yousef on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BGTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
